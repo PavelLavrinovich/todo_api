b4cb6bc44ef7827574f380137b976991825b281c
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2017-08-24 23:43:05 +0300

    implementation and small fixes

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  it { should belongs_to(:todo) }
+  it { should belong_to(:todo) }
 
-  it { should validate_prescence_of(:text) }
+  it { should validate_presence_of(:text) }
 end
